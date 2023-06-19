47c7f08d67b6e9a18852578a385d05b253dbf7e7
LunaTheSnake <109224823+LunaTheSnake@users.noreply.github.com>
2023-06-18 21:15:34 -0300

    Add files via upload

---
+++ endstar.py
@@ -0,0 +1,10 @@
+{
+    "data": {
+        "distance": 1.5813e-05,
+        "gravity": 274.2691614595739,
+        "mass": 1.0,
+        "name": "Sun",
+        "radius": 1.0
+    },
+    "message": "success"
+}
